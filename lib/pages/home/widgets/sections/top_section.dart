ca2b6c190998c6534d33c02d6e56a0ce2e208e90
João Gabriel <jgmoura.dev@gmail.com>
2023-01-12 12:03:47 -0300

    Criando seçâo do topo para tablet

--- lib/pages/home/widgets/sections/top_section.dart
+++ lib/pages/home/widgets/sections/top_section.dart
@@ -1,3 +1,4 @@
+import 'package:clone_udemy/brakpoints.dart';
 import 'package:flutter/material.dart';
 
 import '../custom_search_field.dart';
@@ -18,7 +19,7 @@ class TopSection extends StatelessWidget {
                 AspectRatio(
                   aspectRatio: 3.4,
                   child: Image.network(
-                    'https://images.pexels.com/photos/887751/pexels-photo-887751.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                    'https://images.pexels.com/photos/574080/pexels-photo-574080.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
                     fit: BoxFit.cover,
                   ),
                 ),
@@ -61,6 +62,57 @@ class TopSection extends StatelessWidget {
           );
         }
 
+        if (maxWidth >= mobileBreakPoint) {
+          return SizedBox(
+            height: 320,
+            child: Stack(
+              children: [
+                SizedBox(
+                  height: 250,
+                  width: double.infinity,
+                  child: Image.network(
+                    'https://images.pexels.com/photos/574080/pexels-photo-574080.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  left: 20,
+                  top: 20,
+                  child: Card(
+                      color: Colors.black,
+                      elevation: 8,
+                      child: Container(
+                        padding: const EdgeInsets.all(22),
+                        width: 350,
+                        child: Column(
+                          children: const [
+                            Text(
+                              'Aprenda Flutter com este curso',
+                              style: TextStyle(
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            Text(
+                              'Bora aprender Flutter com o professor Neymar Jr! Cursos por apenas R\$22,90. Qualidade garantida.',
+                              style:
+                                  TextStyle(fontSize: 15, color: Colors.white),
+                            ),
+                            SizedBox(
+                              height: 16,
+                            ),
+                            CustomSearchField()
+                          ],
+                        ),
+                      )),
+                )
+              ],
+            ),
+          );
+        }
         return Container();
       },
     );
