ff70008d80b9771054d0442a1159b4edca5e74c9
João Gabriel <jgmoura.dev@gmail.com>
2023-01-13 11:36:26 -0300

    Utilizando o AutoSizeText

--- lib/pages/home/widgets/course_item.dart
+++ lib/pages/home/widgets/course_item.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class CourseItem extends StatelessWidget {
@@ -15,17 +16,22 @@ class CourseItem extends StatelessWidget {
         const SizedBox(
           height: 4,
         ),
-        const Text(
-          'Criação de Apps Android e iOS com Flutter',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 15,
-            color: Colors.white,
+        const Flexible(
+          child: AutoSizeText(
+            'Criação de Apps Android e iOS com Flutter',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 15,
+              color: Colors.white,
+            ),
           ),
         ),
         const Text(
           'Neymar Jr',
           style: TextStyle(color: Colors.grey),
+        ),
+        const SizedBox(
+          height: 4,
         ),
         const Text(
           'R\$22,90',
